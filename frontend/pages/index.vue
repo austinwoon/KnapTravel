d2a05ef2687ec17fcc5ef561fcee11659d4b31d8
austinwoon <austinwoon.2017@smu.edu.sg>
2020-11-02 20:42:19 +0800

    Basic shit

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="container">
     <div>
-      <a-card title="Travel Destination">
-        <a-form-model :model="form" layout="horizontal" :labelCol="labelCol" :wraperCol="wrapperCol">
+      <a-card title="Travel Destination" class="card-style">
+        <a-form-model layout="vertical" :model="form">
           <a-form-model-item label="City">
-            <a-input v-model="form.city"/>
+            <a-select v-model="form.city" placeholder="Please select city to visit">
+              <template v-for="city in cities">
+                <a-select-option value="city">
+                  {{ city }}
+                </a-select-option>
+              </template>
+            </a-select>
           </a-form-model-item>
 
           <a-form-model-item label="Length of Stay (Days)">
             <a-input-number v-model="form.lengthOfStay" :min="1"/>
           </a-form-model-item>
 
-          <div>
-            <span>Activity Interests: </span>
+          <a-form-model-item label="Activity Interests">
             <template v-for="tag in form.tags">
               <a-checkable-tag :key="tag"
                                :checked="form.selectedTags.indexOf(tag) > -1"
@@ -21,7 +26,23 @@
                 {{ tag }}
               </a-checkable-tag>
             </template>
-          </div>
+          </a-form-model-item>
+
+          <a-form-model-item>
+            <NuxtLink to="/results">
+              <a-button
+                  type="primary"
+                  style="margin-right: 16px"
+                  @click="submitForm"
+              >
+                Generate Itinerary
+              </a-button>
+            </NuxtLink>
+
+            <a-button>
+              Cancel
+            </a-button>
+          </a-form-model-item>
 
         </a-form-model>
       </a-card>
@@ -33,26 +54,69 @@
 export default {
   data() {
     return {
-      labelCol: {span: 4},
-      wrapperCol: {span: 14},
+      cities: [
+        "Tokyo"
+      ],
       form: {
         city: "",
         lengthOfStay: 1,
-        interests: [],
-        tags: ["Sports", "Outdoors", "Family Friendly", "Restaurants", "Food", "Hiking", "Adventure"],
+        tags: ['sports',
+          'sport venue',
+          'drinks',
+          'music venue',
+          'cuisine',
+          'bridge',
+          'amusement ride',
+          'character',
+          'university',
+          'theatre',
+          'skyscraper',
+          'options for meal times',
+          'places to eat and drink',
+          'eat and drink',
+          'area',
+          'buddhist architecture',
+          'club',
+          'shopping centre',
+          'neighbourhood',
+          'shrine',
+          'dancing',
+          'amusement parks',
+          'temple',
+          'department store',
+          'shows, theatres and music',
+          'top attractions',
+          'architectural style',
+          'art museums',
+          'sight',
+          'nightlife',
+          'architect',
+          'park',
+          'relax in a park',
+          'shopping',
+          'museums',
+          'activities',
+          'districts',
+          'sightseeing'],
         selectedTags: [],
       }
     }
   },
   methods: {
+    submitForm() {
+      // TODO(Austin): Add validation if not laze
+      this.$store.commit('form/updateState', this.form);
+    },
     handleChange(tag, checked) {
       const {selectedTags} = this.form;
-      const nextTags = checked ? [...selectedTags, tag] : selectedTags.filter(t => t !== tag);
-      this.form.selectedTags = nextTags;
+      this.form.selectedTags = checked ? [...selectedTags, tag] : selectedTags.filter(t => t !== tag);
     }
   }
 }
 </script>
 
 <style>
+.card-style {
+  max-width: 50%;
+}
 </style>
