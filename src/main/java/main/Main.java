0d556aaf212b9f5adf361aae631225dcc4b7f72c
Huan Lin <thuanlin98@gmail.com>
2020-11-05 01:39:18 +0800

    greedy router

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,10 +1,12 @@
 package main;
 
+import entities.Coordinate;
 import entities.Location;
 import kmeans.Kmeans;
 import location_selector.GreedyLocationSelector;
 import location_selector.KnapsackLocationSelector;
 import location_selector.LocationSelector;
+import routing.GreedyRouter;
 import scoring.ScoringCalculator;
 
 import java.util.HashMap;
@@ -22,18 +24,41 @@ public class Main {
         List<Location> locations = scorer.getLocations();
         Map<Integer, List<Location>> clusters = Kmeans.fit(locations, 5, 100000);
 
-        LocationSelector knapper = new KnapsackLocationSelector(clusters.get(2), 9);
-        LocationSelector greedy = new GreedyLocationSelector(clusters.get(2), 9);
+        getClusterRoutes(getKnapsackLocation(clusters, 9), scorer.getCenter());
+//        LocationSelector knapper = new KnapsackLocationSelector(clusters.get(2), 9);
+////        LocationSelector greedy = new GreedyLocationSelector(clusters.get(2), 9);
 
-        System.out.println(getGreedyLocation(clusters, 9));
-        System.out.println(getKnapsackLocation(clusters, 9));
+//        System.out.println(getGreedyLocation(clusters, 9));
+//        System.out.println(getKnapsackLocation(clusters, 9));
+    }
+
+    public static Map<Integer, List<Location>> getClusterRoutes(Map<Integer, List<Location>> knappedClusters, Coordinate startPoint) {
+        Map<Integer, List<Location>> routedClusters = new HashMap<>();
+        for (Integer i : knappedClusters.keySet()) {
+            List<Location> topClusterLocations = knappedClusters.get(i);
+            System.out.println("for cluster " + i + ": ");
+            System.out.println("before routing: ");
+            for (Location loc : topClusterLocations) {
+                System.out.println(loc.getName());
+            }
+            System.out.println("-------------------");
+            System.out.println("after routing: ");
+            GreedyRouter router = new GreedyRouter(startPoint, topClusterLocations);
+            List<Location> routedLocations = router.getRoute();
+            for (Location loc : routedLocations) {
+                System.out.println(loc.getName());
+            }
+            System.out.println("##################");
+            routedClusters.put(i, routedLocations);
+        }
+        return routedClusters;
     }
 
     public static Map<Integer, List<Location>> getGreedyLocation(Map<Integer, List<Location>> clusters, int timeConstraint) {
         Map<Integer, List<Location>> selectedLocations = new HashMap<>();
         double score = 0.0;
         for (Integer i : clusters.keySet()) {
-            LocationSelector selector = new GreedyLocationSelector(clusters.get(i), 9);
+            LocationSelector selector = new GreedyLocationSelector(clusters.get(i), timeConstraint);
             selectedLocations.put(i, selector.selectLocationsToVisit());
             score += getTotalScore(selector.selectLocationsToVisit());
         }
@@ -46,7 +71,7 @@ public class Main {
         Map<Integer, List<Location>> selectedLocations = new HashMap<>();
         double score = 0.0;
         for (Integer i : clusters.keySet()) {
-            LocationSelector selector = new KnapsackLocationSelector(clusters.get(i), 9);
+            LocationSelector selector = new KnapsackLocationSelector(clusters.get(i), timeConstraint);
             selectedLocations.put(i, selector.selectLocationsToVisit());
             score += getTotalScore(selector.selectLocationsToVisit());
         }
--- src/main/java/scoring/ScoringCalculator.java
+++ src/main/java/scoring/ScoringCalculator.java
@@ -12,6 +12,7 @@ public class ScoringCalculator {
     private List<Location> locations;
     private HashSet<String> preferences;
     private String filePath;
+    private Coordinate center;
 
     private static final double PREFERENCE_WEIGHT = 1.5;
     private static final double POPULAR_WEIGHT = 1.2;
@@ -34,6 +35,10 @@ public class ScoringCalculator {
     private void generateLocationsWithScores() {
         JsonReader jr = new JsonReader(filePath);
         List<JSONObject> data = jr.getContents();
+
+        double latSum = 0;
+        double longSum = 0;
+        int locationNum = data.size();
 
         for (JSONObject location : data) {
 
@@ -92,6 +97,9 @@ public class ScoringCalculator {
             double hours = (Double) location.get("hours_spent");
 
             Map<String, Double> cm = (Map<String, Double>) location.get("coordinates");
+            latSum += cm.get("latitude");
+            longSum += cm.get("longitude");
+
             try {
                 Coordinate coordinates = new Coordinate(cm.get("latitude"), cm.get("longitude"));
                 locations.add(new Location(name, coordinates, score, hours, description, website, price, openingHours, address));
@@ -99,11 +107,15 @@ public class ScoringCalculator {
                 System.out.println(location.get("name"));
             }
         }
+
+        this.center = new Coordinate(latSum/locationNum, longSum/locationNum);
     }
 
     public List<Location> getLocations() {
         return locations;
     }
+
+    public Coordinate getCenter() { return center; }
 
     public static Map<String, Integer> getTagCount() {
         return tagCount;
