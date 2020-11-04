b722baa0d67983ceec556b444c23f42287d79cc8
austinwoon <austinwoon.2017@smu.edu.sg>
2020-11-04 15:40:26 +0800

    Adding initial kmeans classes for huanlin reference

--- frontend/pages/results.vue
+++ frontend/pages/results.vue
@@ -7,51 +7,51 @@
 <script>
 export default {
   data() {
-     return {
-        results: [
-          {
-            day: 1,
-            destinations: [
-              {
-                name: "Asakusa",
-                lat: 130.9,
-                lng: -1.0,
-              },
-              {
-                name: "Shinjuku",
-                lat: 130.9,
-                lng: -1.0,
-              },
-              {
-                name: "Pepe",
-                lat: 130.9,
-                lng: -1.0,
-              },
-              {
-                name: "Asakusa",
-                lat: 130.9,
-                lng: -1.0,
-              },
-              {
-                name: "Asakusa",
-                lat: 130.9,
-                lng: -1.0,
-              },
-            ]
-          },
-          {
-            day: 2,
-            destinations: [
-              {
-                name: "Asakusa",
-                lat: 150.9,
-                lng: -3.0,
-              }
-            ]
-          },
+    return {
+      results: [
+        {
+          day: 1,
+          destinations: [
+            {
+              name: "Asakusa",
+              lat: 130.9,
+              lng: -1.0,
+            },
+            {
+              name: "Shinjuku",
+              lat: 130.9,
+              lng: -1.0,
+            },
+            {
+              name: "Pepe",
+              lat: 130.9,
+              lng: -1.0,
+            },
+            {
+              name: "Asakusa",
+              lat: 130.9,
+              lng: -1.0,
+            },
+            {
+              name: "Asakusa",
+              lat: 130.9,
+              lng: -1.0,
+            },
+          ]
+        },
+        {
+          day: 2,
+          destinations: [
+            {
+              name: "Asakusa",
+              lat: 150.9,
+              lng: -3.0,
+            }
+          ]
+        },
 
-        ]
-     }
+      ]
+    }
   },
   name: "results",
   mounted: {
